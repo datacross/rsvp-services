4d48172222949b6f451a68845a73d0075e11ea90
Quincy Freeman <support@datacross.us>
2011-08-11 08:25:10 +0200

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RsvpServices::Application.config.secret_token = '0a769bdedd717190c2407061a28389a87003a9a678485b5c7a9af34d7be0dd48b6bc6bb419e61cca2c517d8d792815b114e7c52762f251f09fa16e0865c547e9'
